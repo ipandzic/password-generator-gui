66467ed7455389d438195c8b311386853e5bbf12
ipandzicd <ivan.pandzic@gmail.com>
2023-02-21 07:01:17 +0100

    Add form elements

--- PasswordGenerator/Form1.Designer.cs
+++ PasswordGenerator/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox10Chars = new System.Windows.Forms.CheckBox();
+            this.checkBoxCaps = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(224, 98);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(208, 23);
+            this.textBoxPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password Generator";
+            // 
+            // checkBox10Chars
+            // 
+            this.checkBox10Chars.AutoSize = true;
+            this.checkBox10Chars.Location = new System.Drawing.Point(225, 166);
+            this.checkBox10Chars.Name = "checkBox10Chars";
+            this.checkBox10Chars.Size = new System.Drawing.Size(117, 19);
+            this.checkBox10Chars.TabIndex = 2;
+            this.checkBox10Chars.Text = "Use 10 characters";
+            this.checkBox10Chars.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCaps
+            // 
+            this.checkBoxCaps.AutoSize = true;
+            this.checkBoxCaps.Location = new System.Drawing.Point(224, 207);
+            this.checkBoxCaps.Name = "checkBoxCaps";
+            this.checkBoxCaps.Size = new System.Drawing.Size(118, 19);
+            this.checkBoxCaps.TabIndex = 3;
+            this.checkBoxCaps.Text = "Use capital letters";
+            this.checkBoxCaps.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(225, 256);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 19);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Use numbers";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(224, 295);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(92, 19);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Use symbols";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(222, 350);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(246, 50);
+            this.GenerateButton.TabIndex = 6;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(565, 350);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(125, 50);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBoxCaps);
+            this.Controls.Add(this.checkBox10Chars);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBoxPassword;
+        private Label label1;
+        private CheckBox checkBox10Chars;
+        private CheckBox checkBoxCaps;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private Button GenerateButton;
+        private Button saveButton;
     }
 }
